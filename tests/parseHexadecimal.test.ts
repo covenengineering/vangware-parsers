0b95d64ff01cfb1343aef176741691900b4278f5
Luke Shiru <hi@luke.sh>
2022-08-22 01:19:37 -0700

    :arrow_up: dependency upgrade. :white_check_mark: update tests for new `@vangware/test`.

--- tests/parseHexadecimal.test.ts
+++ tests/parseHexadecimal.test.ts
@@ -5,19 +5,19 @@ export default [
 	{
 		given: "a valid hexadecimal string",
 		must: "return correct value",
-		received: parseHexadecimal("101"),
-		wanted: 0x101,
+		received: () => parseHexadecimal("101"),
+		wanted: () => 0x101,
 	},
 	{
 		given: "a hexadecimal string with a dot",
 		must: "return correct value",
-		received: parseHexadecimal("101.5"),
-		wanted: 0x101,
+		received: () => parseHexadecimal("101.5"),
+		wanted: () => 0x101,
 	},
 	{
 		given: "a invalid string",
 		must: "return correct value",
-		received: parseHexadecimal("invalid"),
-		wanted: undefined,
+		received: () => parseHexadecimal("invalid"),
+		wanted: () => undefined,
 	},
 ] as Tests<number | undefined>;
